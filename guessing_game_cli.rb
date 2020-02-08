c9db2a5f0158f3fa86f24aa5b4522dea0699b260
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-08 18:30:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -26,7 +26,7 @@ guess_random = users_guess
  if input.to_i == guess_random
    guess_correct
  else
-   puts "Sorry! The computer guessed #{number}."
+   puts "Sorry! The computer guessed <number>."
 end
 else
   user_exit
