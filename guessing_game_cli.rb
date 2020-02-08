9b149f9fbbcd7bb7e8bbf571acddac92448c2387
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-08 18:42:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -19,7 +19,17 @@ def user_exit
 end  
 
 
-
+def run_guessing_game
+  random_number = rand(6) + 1
+  input = gets.chomp
+  if input == "exit"
+    puts "Goodbye!"
+  elsif input.to_i == random_number
+    puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{random_number}."
+  end
+end
 
 
 
